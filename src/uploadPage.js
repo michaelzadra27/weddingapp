a10ff71005a67d31c413f2bd7e2fcbb9d70ee70d
michaelzadra27 <michaelzadra27@gmail.com>
2022-03-12 18:07:49 -0800

    upload page

---
+++ src/uploadPage.js
@@ -0,0 +1,55 @@
+import { upload } from '@testing-library/user-event/dist/upload';
+import React from 'react';
+import { useState } from 'react'
+
+function UploadPage() {
+    const [fileUploadState, setFileUploadState] = useState('');
+    const [chosenFile, setChosenFile] = useState('');
+    const [previewSrc, setPreviewSrc] = useState('')
+
+    const handleFileUpload = (e) => {
+        const file = e.target.files[0]
+        preview(file);
+    };
+
+    const preview = (file) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend = () => {
+            setPreviewSrc(reader.result);
+        }
+    }
+
+    const handleFileSubmit = (e) => {
+        e.preventDefault();
+        console.log("insubmit")
+        if (!previewSrc) return;
+        uploadMedia(previewSrc);
+    }
+
+    const uploadMedia = (base64EncodedImage) => {
+        console.log(base64EncodedImage)
+    }
+
+    return (
+        <div>
+            <h1>Brenda & Michael</h1>
+            <h2>Please share your photos with us!</h2>
+            <form onSubmit={handleFileSubmit}>
+                <input type="file"
+                    name="image"
+                    onChange={handleFileUpload}
+                    value={fileUploadState}
+                    className="input-form">
+                </input>
+                <button type="submit" className="submit-btn">Submit</button>
+            </form>
+            {previewSrc && (
+                <img src={previewSrc} alt='something' style={{ height: '300px' }} />
+
+            )}
+        </div>
+    );
+}
+
+export default UploadPage;
