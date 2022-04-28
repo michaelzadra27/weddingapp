8678841cc678a2b62e95357d831068a89804cfb5
michaelzadra27 <michaelzadra27@gmail.com>
2022-04-27 20:45:51 -0700

    working on get request

--- server/server.js
+++ server/server.js
@@ -9,6 +9,17 @@ const port = process.env.port || 3001
 app.use(express.json({ limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true}));
 
+app.get('/api/images', async (req, res) => {
+    console.log("hit")
+    const {resources} = await cloudinary.search.expression('folder:wedding_uploads').sort_by('public_id', 'desc').max_results(30).execute();
+    console.log("done")
+
+    const publicIds = resources.map(file => file.public_id)
+    res.json(publicIds)
+    console.log("files sens")
+    
+})
+
 app.post('/api/upload', async (req, res) => {
     try {
         const fileString = req.body.data;
