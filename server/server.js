158679af527464e3627b701bf762d619a165821f
michaelzadra27 <michaelzadra27@gmail.com>
2022-07-09 16:15:21 -0700

    changing scripts

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+console.log("SERVERRR")
 const express = require('express');
 const app = express();
 const { cloudinary } = require('./utils/cloudinary');
@@ -6,7 +7,7 @@ const path = require('path');
 //get server set up on production or local PORT
 
 const PORT = process.env.PORT || 3001;
-app.use(express.static(__dirname + '/'));
+// app.use(express.static(__dirname + '/'));
 
 app.use(express.json({ limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true}));
@@ -29,6 +30,7 @@ app.get('/api/images', async (req, res) => {
 })
 
 app.post('/api/upload', async (req, res) => {
+    console.log("please upload")
     try {
         const fileString = req.body.data;
         console.log(fileString)
