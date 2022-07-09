d823279518a02b8a8b98c2e7f06a3b25bb1b42cd
michaelzadra27 <michaelzadra27@gmail.com>
2022-07-09 16:51:33 -0700

    react srctip buidl

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 // const { cloudinary } = require('./utils/cloudinary');
 const path = require('path');
+require('dotenv').config();
 
 //get server set up on production or local PORT
 
