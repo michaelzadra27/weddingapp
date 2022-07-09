00f95c5a0541123f47ed3500b67aca60c8b4a97d
michaelzadra27 <michaelzadra27@gmail.com>
2022-07-09 16:33:19 -0700

    checking cloud

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 console.log("SERVERRR")
 const express = require('express');
 const app = express();
-const { cloudinary } = require('./utils/cloudinary');
+// const { cloudinary } = require('./utils/cloudinary');
 const path = require('path');
 
 //get server set up on production or local PORT
@@ -16,35 +16,35 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('/api/images', async (req, res) => {
-    console.log("hit")
-    const {resources} = await cloudinary.search.expression('folder:weddingapp').sort_by('public_id', 'desc').max_results(30).execute();
+// app.get('/api/images', async (req, res) => {
+//     console.log("hit")
+//     const {resources} = await cloudinary.search.expression('folder:weddingapp').sort_by('public_id', 'desc').max_results(30).execute();
     
-    // .then(result=>console.log(result)
-    // console.log("done")
+//     // .then(result=>console.log(result)
+//     // console.log("done")
 
-    const publicIds = resources.map(file => file.public_id)
-    res.json(publicIds)
+//     const publicIds = resources.map(file => file.public_id)
+//     res.json(publicIds)
     
     
-})
+// })
 
-app.post('/api/upload', async (req, res) => {
-    console.log("please upload")
-    try {
-        const fileString = req.body.data;
-        console.log(fileString)
-        const uploadedMedia = await cloudinary.uploader.upload(fileString, {
-            upload_preset: 'wedding_uploads'
-        })
-        console.log(uploadedMedia);
-        res.json({msg: 'yayaa'})
+// app.post('/api/upload', async (req, res) => {
+//     console.log("please upload")
+//     try {
+//         const fileString = req.body.data;
+//         console.log(fileString)
+//         const uploadedMedia = await cloudinary.uploader.upload(fileString, {
+//             upload_preset: 'wedding_uploads'
+//         })
+//         console.log(uploadedMedia);
+//         res.json({msg: 'yayaa'})
 
-    } catch (error) {
-        console.error(error)
-        res.status(500).json({err: 'something is off'})
-    }
-})
+//     } catch (error) {
+//         console.error(error)
+//         res.status(500).json({err: 'something is off'})
+//     }
+// })
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
