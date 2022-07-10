5df90b5b768a32851c73a3e4096f6d0a524c88ec
michaelzadra27 <michaelzadra27@gmail.com>
2022-07-09 17:03:45 -0700

    adding postbuild

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json({ limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true}));
+app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
