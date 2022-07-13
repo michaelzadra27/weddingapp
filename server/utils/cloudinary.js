de26d0634b6e7102e514c20976f3703b5499d829
michaelzadra27 <michaelzadra27@gmail.com>
2022-07-12 20:52:58 -0700

    reverting temp fix

--- server/utils/cloudinary.js
+++ server/utils/cloudinary.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
 cloudinary.config({
     cloud_name: "dcteqhlcf",
-    api_key: "635153873816949",
-    api_secret: "KqkykPYcKAw3_WZN5r8JrMBKkBM",
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
     
 });
 
